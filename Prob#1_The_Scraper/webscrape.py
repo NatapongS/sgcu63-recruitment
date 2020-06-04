cd0b9ca63e77bfd34b0f08d00bd645596f0aa720
NatapongS <natapong099@gmail.com>
2020-06-04 13:23:58 +0700

    Make Problem 1 able to open from home page

--- Prob#1_The_Scraper/webscrape.py
+++ Prob#1_The_Scraper/webscrape.py
@@ -4,24 +4,9 @@ from bs4 import BeautifulSoup
 import urllib
 import time
 import json
-
-def getBaanPage():
-    return 'https://rubnongkaomai.com/baan'
-
-
-
-path = pyder.install(browser=pyder.chrome, file_directory='./lib/', verbose=True, chmod=True, overwrite=False, version=None, filename=None, return_info=False)
-driver = webdriver.Chrome(executable_path=path)
-url = getBaanPage()
-driver.get(url)
-baanTable = []
 from selenium.webdriver.common.keys import Keys
 
-def clickBaan(driver, baanSize):
-    allClick = driver.find_elements_by_xpath("//*[contains(text()," + baanSize +')]')
-    for gonnaClick in allClick:
-        gonnaClick.click()
-    
+def scrollAll(driver):
     html = driver.find_element_by_tag_name('html')
     SCROLL_PAUSE_TIME = 1
 
@@ -41,7 +26,29 @@ def clickBaan(driver, baanSize):
             break
         last_height = new_height
     html.send_keys(Keys.HOME)
+
+def clickBaan(driver, baanSize):
+    allClick = driver.find_elements_by_xpath("//*[contains(text()," + baanSize +')]')
+    for gonnaClick in allClick:
+        gonnaClick.click()
+    scrollAll(driver)
     return
+
+
+path = pyder.install(browser=pyder.chrome, file_directory='./lib/', verbose=True, chmod=True, overwrite=False, version=None, filename=None, return_info=False)
+driver = webdriver.Chrome(executable_path=path)
+url = 'https://rubnongkaomai.com'
+driver.get(url)
+allClick = driver.find_elements_by_xpath("//a[contains(@href, /BAAN)]")
+time.sleep(1)
+print(len(allClick)) 
+
+for baanClick in allClick:
+    if baanClick.text == 'BAAN':
+        baanClick.click()
+        break
+
+baanTable = []
 clickBaan(driver,"'บ้านขนาดเล็ก (S)'")
 time.sleep(1)
 clickBaan(driver,"'บ้านขนาดกลาง (M)'")
@@ -58,7 +65,7 @@ for link in allLink:
         if(link['href'].find('json') != -1 and link['href'].find('baan') != -1):
             allBaan.append(link['href'])
 allBaan = list(set(allBaan))
-print(len(allBaan))
+print("number of Baan: " + str(len(allBaan)))
 for baan in allBaan:
     print(baan)
     jsonFile = urllib.request.urlopen('https://rubnongkaomai.com/' + baan).read()
@@ -70,7 +77,6 @@ for baan in allBaan:
     except:
         print("No nameTH or sloganTH")
     time.sleep(0.1)
-print(baanTable)
 table = open('table.html', 'w')
 tableFile ="""<html>
                 <head>
@@ -86,7 +92,7 @@ tableFile ="""<html>
                     </style>
                 </head>
                 <body>
-                    <table style = "width:100%%">
+                    <table>
                         <tr>
                             <th> ชื่อบ้าน </th>
                             <th> สโลแกน </th>
@@ -102,6 +108,7 @@ for baanDetail in baanTable:
     tableContent += "<td>" + baanDetail[0] + "</td>"
     tableContent += "<td>" + baanDetail[1] + "</td>"
     tableContent += "</tr>"
+    print( baanDetail[1])
 whole = tableFile % tableContent
 table.write(whole)
 table.close()
